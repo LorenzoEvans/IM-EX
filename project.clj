5be11b8c82893a03ad2f5232fb3cbeb135e9a3e4
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-09-19 19:02:56 -0400

    Set up ready to go.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.9.1"
+  :main im-gal.core
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]
@@ -13,12 +14,12 @@
   :plugins [[lein-figwheel "0.5.19"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
 
-  :source-paths ["src"]
+  :source-paths ["src" "target"]
   :aliases {"fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]}
 
   :cljsbuild {:builds
               [{:id "dev"
-                :source-paths ["src"]
+                :source-paths ["src" "target"]
 
                 ;; The presence of a :figwheel configuration here
                 ;; will cause figwheel to inject the figwheel client
@@ -51,20 +52,20 @@
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
-             
+
              :css-dirs ["resources/public/css"] ;; watch and update CSS
-             
+
              ;; Start an nREPL server into the running figwheel process
              ;; :nrepl-port 7888
-             
+
              ;; Server Ring Handler (optional)
              ;; if you want to embed a ring handler into the figwheel http-kit
              ;; server, this is for simple ring servers, if this
-             
+
              ;; doesn't work for you just run your own server :) (see lein-ring)
-             
+
              ;; :ring-handler hello_world.server/handler
-             
+
              ;; To be able to open files in your editor from the heads up display
              ;; you will need to put a script on your path.
              ;; that script will have to take a file path and a line number
@@ -73,16 +74,16 @@
              ;; emacsclient -n +$2 $1
              ;;
              ;; :open-file-command "myfile-opener"
-             
+
              ;; if you are using emacsclient you can just use
              ;; :open-file-command "emacsclient"
-             
+
              ;; if you want to disable the REPL
              ;; :repl false
-             
+
              ;; to configure a different figwheel logfile path
              ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             
+
              ;; to pipe all the output to the repl
              ;; :server-logfile false
              }
--- src/im_gal/core.cljs
+++ src/im_gal/core.cljs
@@ -1,15 +1,15 @@
-(ns im-gal.core
+(ns ^:figwheel-always im-gal.core
   (:require))
 
 (enable-console-print!)
 
 (println "This text is printed from src/im-gal/core.cljs. Go ahead and edit it and see reloading in action.")
 
-(js/console.log "Hello, Github Pages!")
+(js/console.log "Hello, Gitsdadasdadsadfsfdhub Pages!")
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello New world!"}))
 
 (defn on-js-reload [])
   ;; optionally touch your app-state to force rerendering depending on
