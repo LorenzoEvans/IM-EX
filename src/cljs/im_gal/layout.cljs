149a249cd9f0dd82edd6dd7966ee768108c9d0eb
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-09-26 22:38:50 -0400

    Border hover solved.

--- src/cljs/im_gal/layout.cljs
+++ src/cljs/im_gal/layout.cljs
@@ -1,14 +1,14 @@
 (ns im-gal.layout
   (:require
    [herb.core :as h :refer [<class]]
-   [im-gal.styles :as styles]
+   [im-gal.styles :as styles :refer [nav-div
+                                     nav-div-2
+                                     nav-nav
+                                     nav-h1
+                                     nav-anchor]]
    [tincture.container]))
 
 
-; (defn bg-border []
-;   ^{:pseudo 
-;     {:hover 
-;      {:border-radius "0.25rem"}}})
 
 
 (defn link-border
@@ -16,17 +16,15 @@
   {:border "1px solid #999999"})
 
 
-
 (defn nav []
-  [:div.bg-near-white.w-third.bottom
-   [:div.near-black.avenir.f2.f-sub-headline.lh-title.db.bg-washed-gray
-    [:nav {:class "flex column justify-center ma3 flex-wrap"}
-     [:h1.fw2.f-sub-headline.tc "The Immutable Gallery"]
-    [:a.no-underline.fw2.link.dib.f2.bg-near-white.avenir.hover-washed-blue.navy.grow.glow.bg-animate.hover-bg-near-black.br3.ba2.bw2.pa2.w-100.tc.h3.ma1
-     {:class (<class link-border) :href "/about"} "About"]
-    [:a.no-underline.fw2.link.dib.f2.bg-near-white.avenir.hover-washed-blue.navy.grow.glow.bg-animate.hover-bg-near-black.br3.ba2.bw2.pa2.w-100.tc.h3.ma1
-     {:class (<class link-border) :href "/contact"} "Contact"]]
-    ]])
+  [:div {:class nav-div}
+   [:div {:class nav-div-2}
+    [:nav {:class nav-nav}
+     [:h1 {:class nav-h1} "The Immutable Gallery"]
+     [:a {:class (str (<class link-border) nav-anchor "hover-border") :href "/about"} "About"]
+     [:a {:class (str (<class link-border) nav-anchor "hover-border") :href "/contact"} "Contact"]]]])
+
+
 
 (defn middle-bar []
   [:div.bg-near-black.w-third.ba.bw3.washed-blue])
@@ -39,17 +37,15 @@
    [:h1.f-sub-headline.lh-title.avenir.f2 "This is the About Page."]
 
    [:div
-    [:a.no-underline.link.dib.mr5.ml5.f2.bg-near-white.avenir.hover-washed-blue.navy.grow.glow.bg-animate.hover-bg-near-black.br3.ba2.bw2.pa2.h3.w-30.tc {:href "/"}
+    [:a {:class (str (<class link-border) nav-anchor "hover-border") :href "/"}
      "go to Home Page"]]])
 
 (defn contact []
   [:div.bg-near-white.w-100
    [:h1.f-sub-headline.lh-title.avenir.f2 "This is the Contact Page"]
    [:div
-    [:a.no-underline.link.dib.mr5.ml5.f2.bg-near-white.avenir.hover-washed-blue.navy.grow.glow.bg-animate.hover-bg-near-black.br3.ba2.bw2.pa2.h3.w-30.tc {:href "/"}
+    [:a{:class (str (<class link-border) nav-anchor "hover-border") :href "/"}
      "go to Home Page"]]])
-
-
 
 (defn home []
   [:div.flex.row.justify-center.bg-near-white.w-100.purple.bw3.vh-100
--- src/cljs/im_gal/styles.cljs
+++ src/cljs/im_gal/styles.cljs
@@ -3,8 +3,7 @@
 
 (defglobal global
   [:body {:margin 0, :min-height "100%"}]
-  [:html {:min-height "100vh"}]
-  )
+  [:html {:min-height "100vh"}])
 
 (defn height []
   {:height "100vh"})
@@ -12,3 +11,15 @@
 
 ; (defn app []
 ;   {})
+
+;; im-gal.layout nav component classnames for Tachyons.
+
+
+(defn new-string [x]
+  (clojure.string/replace x "." " "))
+
+(def nav-div (new-string "bg-near-white.w-third.bottom"))
+(def nav-div-2 (new-string "near-black.avenir.f2.f-sub-headline.lh-title.db.bg-washed-gray"))
+(def nav-nav (new-string "flex.column.justify-center.ma3.flex-wrap"))
+(def nav-h1 (new-string "fw2.f-sub-headline.tc"))
+(def nav-anchor (new-string " no-underline.fw2.link.dib.f2.bg-near-white.avenir.hover-washed-blue.ba.bw1.hover-purple.navy.grow.glow.bg-animate.hover-bg-near-black.br3.pa2.w-100.tc.h3.ma1."))
