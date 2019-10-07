ffddd7283b76c384711a6894b501331a2a387af7
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-10-06 20:09:31 -0400

    Oh...oh boy

--- src/cljs/im_gal/components/pieces/d3_1.cljs
+++ src/cljs/im_gal/components/pieces/d3_1.cljs
@@ -1,9 +1,9 @@
 (ns im-gal.components.pieces.d3-1
   (:require [cljsjs.d3]))
 
-(def data ["pink", "skyblue", "deepskyblue",
-           "royalblue", "steelblue", "lavender",
-           "violet", "pinks", "deeppink"])
+(def data ["gainsboro", "skyblue", "deepskyblue",
+           "indigo", "steelblue", "lavender",
+           "violet", "magenta", "deeppink" "slategray"])
 
 (def width 500)
 (def height 500)
@@ -12,9 +12,32 @@
 (def circumference (Math.PI))
 (def space (/ circumference lines))
 
-(defn linesdata []
+
+(defn linesdata 
   (-> js/d3
-      (.range lines)))
+      (.range lines)
+      (.map (fn [d i]
+              (js-obj 
+               {"startAngle" (* i space)
+                "endAngle" (* (+ linethickness (* i space)) 0.5)
+                "outerRadius" (* (/ width 20) (/ i 100))
+                "strokeWidth" 0.05
+                "fillopacity" (get data (% i 10))})))))
+
+(defn segment [segment path fill class]
+  (-> svg
+      (.selectAll segment)
+      (.data linesdata)
+      (.enter)
+      (.append path)
+      (.attr class segment)
+      (.style fill (fn [d] d.fillcolor))
+      (.style)))
+
+(def d3arc []
+  (-> js/d3
+      (.arc)
+      (.innerRadius 50)))
 
 (defn append-svg []
   (-> js/d3
@@ -23,3 +46,9 @@
       (.attr "height" height)
       (.attr "width" width)
       (.attr "transform" "translate('+width/2+','+height/2+')")))
+
+
+(defn ^:export main []
+  )
+
+(defn on-js-reload [])
