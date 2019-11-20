16b365afb209f78e923165925df463d7e2c7f8d9
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-11-20 18:35:18 -0500

    building up sidebar

--- src/cljs/im_gal/layout.cljs
+++ src/cljs/im_gal/layout.cljs
@@ -17,16 +17,17 @@
 (defn nav []
   [:div {:class nav-div}
    [:div.vh-100.flex.flex-column.justify-between {:class nav-div-2}
-    [:nav.vh-100 {:class nav-nav}
-     [:h1 {:class nav-h1} "Lorenzo Evans"
-      [:span [:h3.mr4 {:class nav-h3}] "Programmer, creative, writer."]]
-     [:a {:class (str (<class link-border) nav-anchor "hover-border") :href "/about"} "About"]
-     [:a {:class (str (<class link-border) nav-anchor "hover-border") :href "/contact"} "Contact"]]]])
+    [:nav.vh-100.shadow-5.w-100 {:class nav-nav}
+     [:h1.f2-m.bb.bw2.bg-near-white.white {:class nav-h1} "Lorenzo Evans"]
+      [:div.flex.flex-column.justify-evenly
+       [:span.bb.bw2.f-subheadline.f2-m.pa1.avenir.fw4.hover.dim [:h3.ma4 {:class nav-h3}] "Programmer."]
+       [:span.bb.bw2.f-subheadline.f2-m.pa1.avenir.fw4.hover.dim [:h3.ma4 {:class nav-h3}] "Creative."]
+       [:span.bb.bw2.f-subheadline.f2-m.pa1.avenir.fw4.hover.dim [:h3.ma4 {:class nav-h3}] "Writer."]]]]])
 
 (defn left-sidebar []
-  [:div.w-two-thirds.h-100.overflow-scroll.avenir.ba
+  [:div.w-100.h-100.overflow-scroll.avenir
    [:section.tc.h-75.bw2.bg-washed-red.navy.f-headline{:class content-area} "Skills"]
-   [:section.self-center.h-75.bw2.bg-washed-blue.navy.f-headline.w-100.fixed {:class content-area} "Experience"]
+   [:section.tc.h-75.bw2.bg-washed-blue.navy.f-headline.w-100.fixed {:class content-area} "Experience"]
    [:section.tc.h-75.bw2.bg-washed-near-white.navy.f-headline.fw6 {:class content-area} "Projects"]
    [:section.tc.h-75.bw2.bg-washed-near-white.navy.f-headline.fw6 {:class content-area} "Blog"]
    ])
@@ -47,6 +48,6 @@
      "go to Home Page"]]])
 
 (defn home []
-  [:div.flex.row.justify-center.bg-near-white.w-100.purple.bw3.vh-100
+  [:div.flex.row.justify-center.bg-near-white.w-100.purple.bw3.vh-100.flex-row-m
    [nav]
    [left-sidebar]])
