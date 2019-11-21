9cb774fba755cf959ba6d98d0df25d317258afed
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-11-20 21:43:00 -0500

    Oh this might be saucy.

--- src/cljs/im_gal/layout.cljs
+++ src/cljs/im_gal/layout.cljs
@@ -14,23 +14,28 @@
   []
   {:border "2px solid #999999"})
 
+(defn width []
+  {:width " 40% "})
+(defn width-2 []
+  {:width " 60% "})
 (defn nav []
-  [:div {:class nav-div}
+  [:div.bg-white.fixed.z-2 {:class (str nav-div (<class width)) }
    [:div.vh-100.flex.flex-column.justify-between {:class nav-div-2}
     [:nav.vh-100.shadow-5.w-100 {:class nav-nav}
-     [:h1.f2-m.bb.bw2.bg-near-white.white {:class nav-h1} "Lorenzo Evans"]
-      [:div.flex.flex-column.justify-evenly
-       [:span.bb.bw2.f-subheadline.f2-m.pa1.avenir.fw4.hover.dim [:h3.ma4 {:class nav-h3}] "Programmer."]
-       [:span.bb.bw2.f-subheadline.f2-m.pa1.avenir.fw4.hover.dim [:h3.ma4 {:class nav-h3}] "Creative."]
-       [:span.bb.bw2.f-subheadline.f2-m.pa1.avenir.fw4.hover.dim [:h3.ma4 {:class nav-h3}] "Writer."]]]]])
+[:div.flex.flex-column.justify-evenly.self-center.items-center
+ [:h1.f2-m.bb.bw2.w-100 {:class nav-h1} "Lorenzo Evans"]
+ [:span.bb.bw2.f-subheadline.f2-m.pa1.avenir.fw5.hover.dim.dark-gray [:h3.ma4 {:class nav-h3}] "Programmer."]
+ [:span.bb.bw2.f-subheadline.f2-m.pa1.avenir.fw5.hover.dim.dark-gray [:h3.ma4 {:class nav-h3}] "Creative."]
+ [:span.bb.bw2.f-subheadline.f2-m.pa1.avenir.fw5.hover.dim.dark-gray [:h3.ma4 {:class nav-h3}] "Writer."]]]]])
 
 (defn left-sidebar []
-  [:div.w-100.h-100.overflow-scroll.avenir
-   [:section.tc.h-75.bw2.bg-washed-red.navy.f-headline{:class content-area} "Skills"]
-   [:section.tc.h-75.bw2.bg-washed-blue.navy.f-headline.w-100.fixed {:class content-area} "Experience"]
-   [:section.tc.h-75.bw2.bg-washed-near-white.navy.f-headline.fw6 {:class content-area} "Projects"]
-   [:section.tc.h-75.bw2.bg-washed-near-white.navy.f-headline.fw6 {:class content-area} "Blog"]
-   ])
+  [:div.h-100.overflow-scroll.avenir.w-100 {:class (<class width-2)}
+   [:section.tc.h-75.bw2.bg-washed-red.navy.f-headline.w-100.relative {:class content-area} "Skills"]
+   [:section.tc.h-75.bw2.bg-washed-blue.navy.w-100.fixed {:class content-area} "Experience"]
+   [:section.tc.h-75.bw2.bg-washed-near-white.navy {:class content-area} "Projects"]
+   [:section.tc.h-75.bw2.bg-washed-near-white.navy.absolute.w-100.h-100 {:class content-area} "Blog"]
+   [:section.tc.h-75.bw2.bg-washed-near-white.navy {:class content-area} "Contact"]
+   [:section.h-100 "holup"]])
 
 (defn about []
   [:div.bg-near-white.w-100
@@ -48,6 +53,6 @@
      "go to Home Page"]]])
 
 (defn home []
-  [:div.flex.row.justify-center.bg-near-white.w-100.purple.bw3.vh-100.flex-row-m
+  [:div.flex.row.bg-near-white.w-100.purple.bw3.vh-100.flex-row-m
    [nav]
    [left-sidebar]])
--- src/cljs/im_gal/styles.cljs
+++ src/cljs/im_gal/styles.cljs
@@ -18,10 +18,10 @@
 (defn new-string [x]
   (clojure.string/replace x "." " "))
 
-(def nav-div (new-string "flex.flex-column.justify-between.w-33.vh-100.dark-blue"))
+(def nav-div (new-string " flex.flex-column.justify-between.vh-100.dark-blue "))
 (def nav-div-2 (new-string "avenir.f2.f-sub-headline.lh-title.db.bg-washed-gray"))
 (def nav-nav (new-string "flex.column.justify-center.flex-wrap"))
-(def nav-h1 (new-string "fw6.f-subheadline.tc.navy"))
-(def nav-h3 (new-string "fw6.f-subheadline.tc.hot-pink"))
-(def nav-anchor (new-string "no-underline.fw2.link.dib.f2.bg-near-white.avenir.hover-washed-blue.ba.bw1.hover-purple.navy.grow.glow.bg-animate.hover-bg-near-black.br3.pa2.w-100.tc.h3.ma1."))
-(def content-area (new-string "bw2.bg-washed-green.navy.f1.f-subheadline"))
+(def nav-h1 (new-string "fw6.f-headline.tc.near-black"))
+(def nav-h3 (new-string "fw6.f-subheadline.tc.link."))
+(def nav-anchor (new-string "no-underline.fw3.link.dib.f2.bg-near-white.tracked.self-center.avenir.hover-washed-blue.ba.bw1.hover-purple.navy.grow.glow.bg-animate.hover-bg-near-black.br3.pa2.w-100.tc.h5.ma1."))
+(def content-area (new-string "bw2.bg-washed-green.navy.f-headline.fw6"))
