c573e20eadc6522a9a603488d0326e9cd49a5ead
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-11-25 23:06:10 -0500

    this makes me happy

--- src/cljs/im_gal/components/left_side.cljs
+++ src/cljs/im_gal/components/left_side.cljs
@@ -19,11 +19,11 @@
    "Clojure/Script"])
 
 (defn left-side []
-  [:div.h-100.avenir.w-100.overflow-scroll.z-3.bg-near-white.bl.bw2
-   [:section.tracked.bb.bw2.threed-text.dark-gray.flex.flex-column.items-center.pl5.ml7.h-100.bw2.w-100.justify-between {:class content-area}  "Skills"
-    [:div.flex.flex-row.flex-wrap.w-50.overflow-scroll.justify-evenly.no-shadow
+  [:div.h-100.avenir.overflow-scroll.z-3.bw2.b--near-black.w-60
+   [:section.mb2.tracked.bw2.threed-text.dark-gray.h-100.bw2.w-100.justify-between {:class content-area}  "Skills"
+    [:div.w-60.flex.flex-row.flex-wrap.justify-evenly.no-shadow.h-75
      (for [item skills]
-       [:span.f1.no-shadow.dark-gray.hover-animate.grow.pa3.hover-dark-pink.ba.bw2.b--near-black.ma3 item])]]
-   [:section.tracked.bb.bw2.threed-text.dark-gray.flex.flex-column.items-center.pl5.ml7.h-100.bw2.bg-washed-blue.w-100 {:class content-area} "Experience"]
-   [:section.tracked.bb.bw2.threed-text.dark-gray.flex.flex-column.items-center.pl5.ml7.h-100.bw2.bg-washed-near-white.w-100 {:class content-area} "Projects"]
-   [:section.tracked.bb.bw2.threed-text.dark-gray.flex.flex-column.items-center.pl5.ml7.h-25.bw2.bg-washed-blue.w-100 {:class content-area} "Contact"]])
+       [:span.f1.no-shadow.dark-gray.hover-animate.grow.glow.bg-washed-blue.br1.pa3.hover-dark-pink.ba.bw2.b--near-black.ma3 item])]]
+   [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.items-center.pl5.h-100.w-100.bt.bw2.b--near-white.mt4 {:class content-area} "Experience"]
+   [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.items-center.pl5.h-100.bw2.w-60 {:class content-area} "Projects"]
+   [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.items-center.pl5.h-25.w-60 {:class content-area} "Contact"]])
