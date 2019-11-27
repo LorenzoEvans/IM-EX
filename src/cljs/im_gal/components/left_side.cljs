4cb0164c3cc8b403af9bfc65d32ca3aebf033ce2
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-11-27 17:02:06 -0500

    almo done

--- src/cljs/im_gal/components/left_side.cljs
+++ src/cljs/im_gal/components/left_side.cljs
@@ -25,12 +25,12 @@
      (for [item skills]
        ^{:key item}
        [:span.f1.no-shadow.self-center.dark-gray.hover-animate.grow.glow.bg-washed-blue.br1.pa3.hover-dark-pink.ba.bw2.b--near-black.ma3.f4-m.ma1-m.justify-between-m item])]]
-   [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.items-center.h-100.w-100.bb.bw2.b--near-white.mt5 {:class content-area} "Experience"]
+   [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.h-100.w-100.bb.bw2.b--near-white.mt5 {:class content-area} "Experience"]
    [:section.mb2.tracked.threed-text.dark-gray.h-100.bw2.w-100.b--near-white.mt5.overflow-scroll {:class content-area} "Projects"
     [:h1.tc.f5.ttu.fw6.tracked.mb4.avenir "Directory"]
-     [:div.flex.flex-row.flex-wrap 
-      [:div.w-50.border-box.f1.fw5.overflow-hidden.ba.bw2.white.h-25.ma4.near-black.no-shadow "Maybe Sheeps"]
-      [:div.w-50.border-box.f1.fw5.overflow-hidden.ba.bw2.white.h-25.ma4.near-black.no-shadow "Aunalytix"]
-      [:div.w-50.border-box.f1.fw5.overflow-hidden.ba.bw2.white.h-25.ma4.near-black.no-shadow "Note Taken"]
-      [:div.w-50.border-box.f1.fw5.overflow-hidden.ba.bw2.white.h-25.ma4.near-black.no-shadow "stuff"]
-      [:div.w-50.border-box.f1.fw5.overflow-hidden.ba.bw2.white.h-25.ma4.near-black.no-shadow "stuff"]]]])
+    [:div.flex.flex-row.flex-wrap
+     [:div.w-50.border-box.f1.fw5.overflow-hidden.ba.bw2.white.h-25.ma4.near-black.no-shadow "Maybe Sheeps"]
+     [:div.w-50.border-box.f1.fw5.overflow-hidden.ba.bw2.white.h-25.ma4.near-black.no-shadow "Aunalytix"]
+     [:div.w-50.border-box.f1.fw5.overflow-hidden.ba.bw2.white.h-25.ma4.near-black.no-shadow "Note Taken"]
+     [:div.w-50.border-box.f1.fw5.overflow-hidden.ba.bw2.white.h-25.ma4.near-black.no-shadow "stuff"]
+     [:div.w-50.border-box.f1.fw5.overflow-hidden.ba.bw2.white.h-25.ma4.near-black.no-shadow "stuff"]]]])
--- src/cljs/im_gal/components/nav.cljs
+++ src/cljs/im_gal/components/nav.cljs
@@ -15,9 +15,9 @@
     [:nav.vh-100.w-100 {:class nav-nav}
      [:div.flex.flex-column.justify-between.h-75.self-center.items-center.relative
       [:div.mt3.bw3.f-headline.light-green.grow.glow.hover-animate.o-80.w-80.hover-washed-red.ba.w-90-m.f1-m.fw6.element "Lorenzo Evans"]
-      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red.bounceInElement "Programmer."]
-      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red.bounceInElement "Creative."]
-      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red.bounceInElement "Writer."]
+      [:span.mt3.w-60-m.bw2.w-75.hover-animate.bb.glow.f2.f2-m.pa1.avenir.fw6.washed-green.grow.hover-dark-gray.hover-bg-animated.hover-bg-gray.glow.bounceInElement "Programmer."]
+      [:span.mt3.w-60-m.bw2.w-75.hover-animate.bb.glow.f2.f2-m.pa1.avenir.fw6.washed-green.grow.hover-dark-gray.hover-bg-animated.hover-bg-gray.glow.bounceInElement "Creative."]
+      [:span.mt3.w-60-m.bw2.w-75.hover-animate.bb.glow.f2.f2-m.pa1.avenir.fw6.washed-green.grow.hover-dark-gray.hover-bg-animated.hover-bg-gray.glow.bounceInElement "Writer."]
       [:section.mb2.dark-gray.h-100.w-80.mt5.ba.bw2.b--washed-red.glow.hover-animate.grow.glow.ma4.no-shadow.near-black.flex.flex-column.h-75.justify-between.f2
         [:span.avenir.no-shadow.near-white.f-headline.w-100.bb.bw2.b--dark-gray "Contact"]
         [:a.no-underline.avenir.near-white.f1.hover-bg-washed-blue.glow.no-shadow {:href "mailto:lorenzo.evans94@gmail.com"} "Email"]
