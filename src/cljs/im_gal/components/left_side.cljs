d49734f605980048d2babf819212a74b211fa61a
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-11-27 16:38:10 -0500

    pushing changes

--- src/cljs/im_gal/components/left_side.cljs
+++ src/cljs/im_gal/components/left_side.cljs
@@ -23,6 +23,7 @@
    [:section.mb2.tracked.bw2.threed-text.dark-gray.h-100.bw2.w-100.justify-between.bb {:class content-area}  "Skills"
     [:div.w-100.flex.flex-row.flex-wrap.justify-evenly.no-shadow.h-75.w-80-m
      (for [item skills]
+       ^{:key item}
        [:span.f1.no-shadow.self-center.dark-gray.hover-animate.grow.glow.bg-washed-blue.br1.pa3.hover-dark-pink.ba.bw2.b--near-black.ma3.f4-m.ma1-m.justify-between-m item])]]
    [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.items-center.h-100.w-100.bb.bw2.b--near-white.mt5 {:class content-area} "Experience"]
    [:section.mb2.tracked.threed-text.dark-gray.h-100.bw2.w-100.b--near-white.mt5.overflow-scroll {:class content-area} "Projects"
--- src/cljs/im_gal/components/nav.cljs
+++ src/cljs/im_gal/components/nav.cljs
@@ -5,7 +5,8 @@
                                    nav-h1
                                    nav-h3
                                    width]]
-            [herb.core :refer [<class]]))
+            [herb.core :refer [<class]]
+            ["AOS" :as aos]))
 
 (defn nav []
   [:div.w-40 {:class nav-div}
@@ -13,10 +14,10 @@
    [:div.vh-100.flex.flex-column.justify-between {:class nav-div-2}
     [:nav.vh-100.w-100 {:class nav-nav}
      [:div.flex.flex-column.justify-between.h-75.self-center.items-center.relative
-      [:span.mt3.bw3.f-headline.light-green.grow.glow.hover-animate.bg-mid-gray.o-80.w-80.hover-washed-red.ba.w-90-m.f1-m.fw6.pa1 "Lorenzo Evans"]
-      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red "Programmer."]
-      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red "Creative."]
-      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red. "Writer."]
+      [:div.mt3.bw3.f-headline.light-green.grow.glow.hover-animate.o-80.w-80.hover-washed-red.ba.w-90-m.f1-m.fw6.element "Lorenzo Evans"]
+      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red.bounceInElement "Programmer."]
+      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red.bounceInElement "Creative."]
+      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red.bounceInElement "Writer."]
       [:section.mb2.dark-gray.h-100.w-80.mt5.ba.bw2.b--washed-red.glow.hover-animate.grow.glow.ma4.no-shadow.near-black.flex.flex-column.h-75.justify-between.f2
         [:span.avenir.no-shadow.near-white.f-headline.w-100.bb.bw2.b--dark-gray "Contact"]
         [:a.no-underline.avenir.near-white.f1.hover-bg-washed-blue.glow.no-shadow {:href "mailto:lorenzo.evans94@gmail.com"} "Email"]
