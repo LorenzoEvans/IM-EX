226b4ace623eb0a80208874c67a1734fb6562ee0
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-11-26 22:32:51 -0500

    nothing like a little re structuring

--- src/cljs/im_gal/components/left_side.cljs
+++ src/cljs/im_gal/components/left_side.cljs
@@ -20,10 +20,9 @@
 
 (defn left-side []
   [:div.h-100.avenir.overflow-scroll.z-3.bw2.b--near-black.w-60.ml3-m
-   [:section.mb2.tracked.bw2.threed-text.dark-gray.h-100.bw2.w-100.justify-between {:class content-area}  "Skills"
+   [:section.mb2.tracked.bw2.threed-text.dark-gray.h-100.bw2.w-100.justify-between.bb {:class content-area}  "Skills"
     [:div.w-100.flex.flex-row.flex-wrap.justify-evenly.no-shadow.h-75.w-80-m
      (for [item skills]
        [:span.f1.no-shadow.self-center.dark-gray.hover-animate.grow.glow.bg-washed-blue.br1.pa3.hover-dark-pink.ba.bw2.b--near-black.ma3.f4-m.ma1-m.justify-between-m item])]]
-   [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.items-center.h-100.w-100.bt.bw2.b--near-white.mt4 {:class content-area} "Experience"]
-   [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.items-center.h-100.bw2.w-100 {:class content-area} "Projects"]
-   [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.items-center.h-25.w-100 {:class content-area} "Contact"]])
+   [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.items-center.h-100.w-100.bb.bw2.b--near-white.mt5 {:class content-area} "Experience"]
+   [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.items-center.h-100.bw2.w-100.b--near-white.mt5 {:class content-area} "Projects"]])
--- src/cljs/im_gal/components/nav.cljs
+++ src/cljs/im_gal/components/nav.cljs
@@ -13,7 +13,12 @@
    [:div.vh-100.flex.flex-column.justify-between {:class nav-div-2}
     [:nav.vh-100.w-100 {:class nav-nav}
      [:div.flex.flex-column.justify-between.h-75.self-center.items-center.relative
-      [:span.mt3.bw3.f-headline.light-green.grow.glow.hover-animate.bg-mid-gray.o-80.w-100.hover-washed-red.ba.w-90-m.f1-m.fw6.pa1 "Lorenzo Evans"]
-      [:span.mt3.w-60-m.ba.bw1.w-100.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f-headline.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red "Programmer."]
-      [:span.mt3.w-60-m.ba.bw1.w-100.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f-headline.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red "Creative."]
-      [:span.mt3.w-60-m.ba.bw1.w-100.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f-headline.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red. "Writer."]]]]])
+      [:span.mt3.bw3.f-headline.light-green.grow.glow.hover-animate.bg-mid-gray.o-80.w-80.hover-washed-red.ba.w-90-m.f1-m.fw6.pa1 "Lorenzo Evans"]
+      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red "Programmer."]
+      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red "Creative."]
+      [:span.mt3.w-60-m.ba.bw1.w-75.hover-animate.bb.bw3.bg-mid-gray.o-80.glow.f2.f2-m.pa1.avenir.fw6.light-green.grow.hover-washed-red. "Writer."]
+      [:section.mb2.dark-gray.h-100.w-75.mt5.ba.bw2.b--washed-red.glow.o-90.hover-animate.grow.glow.ma4.no-shadow.near-black.flex.flex-column.h-50.justify-between.f2
+        [:span.avenir.no-shadow.near-white.f-headline.w-75.bb.bw2.b--silver "Contact"]
+        [:span.avenir.no-shadow.near-black.f1 "Email :: lorenzo.evans94@gmail.com ||"
+         [:span [:a.no-underline.avenir.near-white {:href "mailto:lorenzo.evans94@gmail.com"} "Drop a line."]]]
+        [:span.avenir.no-shadow.o-30.near-black.hover-bg-washed-blue.f3 "LinkedIn"]]]]]])
