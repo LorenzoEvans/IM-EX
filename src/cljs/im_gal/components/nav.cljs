97b88d9c366a282bdca37ab8ac6e179a250e6f2d
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-04 16:22:50 -0500

    got to add project info to data structurrrre

--- src/cljs/im_gal/components/nav.cljs
+++ src/cljs/im_gal/components/nav.cljs
@@ -4,6 +4,9 @@
                                    nav-nav
                                    nav-h1
                                    nav-h3
+                                   nav-span
+                                   contact-section
+                                   name-cta
                                    width]]
             [herb.core :refer [<class]]))
 
@@ -13,11 +16,11 @@
    [:div.vh-100.flex.flex-column.justify-between {:class nav-div-2}
     [:nav.vh-100.w-100 {:class nav-nav}
      [:div.flex.flex-column.justify-between.h-75.self-center.items-center.relative
-      [:div.mt3.bw3.f-headline.light-green.grow.glow.hover-animate.o-80.w-80.hover-washed-red.ba.w-90-m.f1-m.fw6.element "Lorenzo Evans"]
-      [:span.mt3.w-60-m.bw2.w-75.hover-animate.bb.glow.f2.f2-m.pa1.avenir.fw6.washed-green.grow.hover-dark-gray.hover-bg-animated.hover-bg-gray.glow.bounceInElement "Programmer."]
-      [:span.mt3.w-60-m.bw2.w-75.hover-animate.bb.glow.f2.f2-m.pa1.avenir.fw6.washed-green.grow.hover-dark-gray.hover-bg-animated.hover-bg-gray.glow.bounceInElement "Creative."]
-      [:span.mt3.w-60-m.bw2.w-75.hover-animate.bb.glow.f2.f2-m.pa1.avenir.fw6.washed-green.grow.hover-dark-gray.hover-bg-animated.hover-bg-gray.glow.bounceInElement "Writer."]
-      [:section.mb2.pa2.dark-gray.h-100.w-80.w-100-m.mt5.ba.bw2.b--washed-red.glow.hover-animate.grow.glow.ma4.no-shadow.near-black.flex.flex-column.h-75.justify-between.f2
+      [:div {:class name-cta} "Lorenzo Evans"]
+      [:span {:class nav-span} "Programmer."]
+      [:span {:class nav-span} "Creative."]
+      [:span {:class nav-span} "Writer."]
+      [:section {:class contact-section}
         [:span.avenir.no-shadow.near-white.f-headline.w-100.bb.bw2.b--dark-gray.f2-m "Contact"]
         [:a.no-underline.ba.bw2.avenir.near-white.f1.glow.no-shadow {:href "mailto:lorenzo.evans94@gmail.com"} "Email"]
        [:a.avenir.ba.bw2.no-shadow.near-white.hover-animate.glow.f1.no-underline {:href "https://www.linkedin.com/in/lorev/"} "LinkedIn"]]]]]])
--- src/cljs/im_gal/styles.cljs
+++ src/cljs/im_gal/styles.cljs
@@ -24,10 +24,13 @@
 (defn new-string [x]
   (clojure.string/replace x "." " "))
 
+(def name-cta "mt3.bw3.f-headline.light-green.grow.glow.hover-animate.o-80.w-80.hover-washed-red.ba.w-90-m.f1-m.fw6.element")
 (def nav-div (new-string " flex.flex-column.justify-between.vh-100.dark-blue "))
 (def nav-div-2 (new-string "avenir.f2.f-sub-headline.lh-title.db.bg-washed-gray"))
 (def nav-nav (new-string "flex.column.justify-center.flex-wrap"))
 (def nav-h1 (new-string "fw6.f-headline.tc.near-black"))
 (def nav-h3 (new-string ""))
+(def nav-span (new-string "mt3.w-60-m.bw2.w-75.hover-animate.bb.glow.f2.f2-m.pa1.avenir.fw6.washed-green.grow.hover-dark-gray.hover-bg-animated.hover-bg-gray.glow.bounceInElement"))
 (def nav-anchor (new-string "no-underline.fw3.link.dib.f2.bg-near-white.tracked.self-center.avenir.hover-washed-blue.ba.bw1.hover-purple.navy.grow.glow.bg-animate.hover-bg-near-black.br3.pa2.w-100.tc.h5.ma1."))
 (def content-area (new-string "f-headline.fw6"))
+(def contact-section (new-string "mb2.pa2.dark-gray.h-100.w-80.w-100-m.mt5.ba.bw2.b--washed-red.glow.hover-animate.grow.glow.ma4.no-shadow.near-black.flex.flex-column.h-75.justify-between.f2"))
