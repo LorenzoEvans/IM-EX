ae4c5dce7cd0ccd79f8ecccd54f115e67897a845
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-04 19:00:17 -0500

    admittedly the big o of this html is bad

--- src/cljs/im_gal/components/left_side.cljs
+++ src/cljs/im_gal/components/left_side.cljs
@@ -20,21 +20,21 @@
    "Clojure/Script"])
 
 (def projects 
-  [(array-map {:name "Maybe Sheeps"}
-    :about "Personal micro-blog for tech-related/miscellaneous things I find interesting, and chronicling my experience and growth with the FP paradigm/as a Clojurist/Lisp hacker."
-    :tech-used "tech info")
+  [ {:name "Maybe Sheeps"
+     :about "Personal micro-blog for tech-related/miscellaneous things I find interesting, and chronicling my experience and growth with the FP paradigm/as a Clojurist/Lisp hacker."
+     :tech-used ["tech info"]}
     
    {:name "Aunalytix"
     :about "Aunalytix will target creative artists (currently audio-focused), who are tired of not having access to data regarding their music, due to the nature of the environment in which said data is generated, and likewise, consumers of said art, who are also tired of having the same experience with their data. The goal of this application is to strike up a synergy between the needs and roles of artists, and their supporters, via the user-centric data handling principles this application will enforce."
-    :tech-used "tech info"}
+    :tech-used ["tech info"]}
     
    {:name "RateMyDIY"
     :about "info"
-    :tech-used "tech info"}
+    :tech-used ["Firebase, GraphQL, React, SCSS"]}
     
    {:name "Note Taken"
     :about  "Desktop application for taking notes/reminders, etc, originally done in React/Node/Express/SQL, re-vamped in Seesaw, a Clojure framework for developing Desktop applications in Swing, JDBC/SQL, features including but not limited to markdown parsing, and export to file extensions."
-    :tech-used "tech info"}])
+    :tech-used ["tech info"]}])
     
    
 (defn left-side []
@@ -45,10 +45,12 @@
        ^{:key item}
        [:span.f1.no-shadow.self-center.dark-gray.hover-animate.grow.glow.bg-washed-blue.br1.pa3.hover-dark-pink.ba.bw2.b--near-black.ma3.f4-m.ma1-m.justify-between-m item])]]
    [:section.mb2.tracked.threed-text.dark-gray.flex.flex-column.h-100.w-100.bb.bw2.b--near-white.mt5 {:class content-area} "Experience"]
-   [:section.mb2.tracked.threed-text.dark-gray.h-100.bw2.w-100.b--near-white.mt5.overflow-scroll {:class content-area} "Projects"]
-   (for [project projects]
+   [:section.mb2.tracked.threed-text.dark-gray.h-100.bw2.w-100.b--near-white.mt5.overflow-scroll {:class content-area} "Projects"
+    (for [project projects]
      [:div.flex.flex-row.flex-wrap
         ^{:key (:name project)}
         [:div {:class project-container} (:name project)
-          [:div {:class project-content} (:about project)]]])])
+          [:div {:class project-content} (:about project)]
+          [:div 
+           (for [item (:tech-used project)] [:li.f5.no-shadow.self-center.dark-gray.hover-animate.grow.glow.bg-washed-blue.br1.pa3.hover-dark-pink.ba.bw2.b--near-black.ma3.ma1-m.justify-between-m item])]]])]])
       
