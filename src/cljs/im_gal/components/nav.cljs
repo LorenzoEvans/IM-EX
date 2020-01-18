98b9c48229eb2b15f1535c9ae56bfdd1f6ba85a6
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-18 16:22:50 -0500

    alright, calling it done

--- src/cljs/im_gal/components/nav.cljs
+++ src/cljs/im_gal/components/nav.cljs
@@ -6,12 +6,12 @@
                                    nav-span
                                    nav-span-2
                                    contact-section
+                                   contact-anchor
                                    width my-name]]
             [herb.core :refer [<class]]))
 
 (defn nav []
-  [:div.w-40 {:class nav-div}
-   [:img.absolute.bg-image]
+  [:div.w-40.br.bw2.b--near-black.bg-near-white {:class nav-div}
    [:div.vh-100.flex.flex-column.justify-between {:class nav-div-2}
     [:nav.vh-100.w-100 {:class nav-nav}
      [:div.flex.flex-column.justify-between.h-75.mt3.items-center.relative
@@ -19,6 +19,5 @@
       [:span {:class nav-span} "Programmer."]
       [:span {:class nav-span} "Creative."]
       [:span {:class nav-span} "Writer."]
-      [:span {:class nav-span-2} "Contact:"]
-      [:a.no-underline.ba.bw2.avenir.near-black.f1.glow.no-shadow.bg-near-white {:class nav-span :href "mailto:lorenzo.evans94@gmail.com"} "Email"]
-      [:a.avenir.ba.bw2.no-shadow.near-black.hover-animate.glow.f1.no-underline.bg-near-white {:class nav-span :href "https://www.linkedin.com/in/lorev/"} "LinkedIn"]]]]])
+      [:a {:class contact-anchor :href "mailto:lorenzo.evans94@gmail.com"} "Email"]
+      [:a {:class contact-anchor :href "https://www.linkedin.com/in/lorev/"} "LinkedIn"]]]]])
