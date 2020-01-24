627981b71407381f1098fb5ce4c44da16071d132
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-24 17:09:31 -0500

    saving changes so far

--- src/cljs/im_gal/components/left_side.cljs
+++ src/cljs/im_gal/components/left_side.cljs
@@ -29,7 +29,8 @@
   [{:name "Synths For Compilers"
      :about "Personal micro-blog for tech-related/miscellaneous things I find interesting, and chronicling my experience and growth with the FP paradigm/as a Clojurist/Lisp hacker."
      :tech-used ["reagent" "tachyons" "hiccup"]
-     :app-link "https://www.synthsforcompilers.dev"}
+     :app-link "https://www.synthsforcompilers.dev"
+     :repo "https://github.com/LorenzoEvans/m-s"}
    {:name "Clojurena"
     :about "A library that acts as a wrapper over the are.na API."
     :tech-used ["Clojure"]
@@ -38,7 +39,8 @@
     :about "RateMyDiy targets people who want to get up and get productive and learn different skills. It utilizes a rating system to analyze the most effective and enjoyed projects, project makers, and reviewers. This will allow the projects and user generated content that is most beneficial to the community to be the most available to people who are looking for high quality information."
     :tech-used ["Firebase" "GraphQL/Apollo" "Prisma" "Sendgrid" "Stripe" "Cloudinary" "React" "SASS"]
     :docs "https://ratemydiy.github.io/Documentation/Architecture.html"
-    :app-link "http://ratemydiy.tk"}
+    :app-link "http://ratemydiy.tk"
+    :repo "https://github.com/Lambda-School-Labs/labspt2-rate-my-diy"}
    {:name "Personote"
     :about  "Desktop note-taking application in Clojure, CLJFX/OpenJFX for GUI programming, JDBC/SQL for persistence."
     :tech-used ["Clojure" "CLJFX/OpenJFX" "JDBC/SQL"]
@@ -53,7 +55,7 @@
                "Received 2.87 / 3 rating as a Team Lead."
                "Piloted new school-wide video feed-back program, via retrospectives covering student/TL experience with program."
                "Took part in daily stand ups, code reviews, on-on-ones, and help desks with Section Leads, fellow TL’s and students."]}])
-
+; change nav, make them links or somethings
 (defn content-side []
   [:div {:class content-container}
    [:section {:class skills-content-area}  "Skills"
@@ -79,6 +81,7 @@
                                        :href (:app-link project)} "Link to project"]
           (cond (not= (:docs project) nil)[:a {:class project-link ; NOTE TO SELF CHANGE THIS CONDITIONAL WHEN YOU ADD MORE DOCUMENTATIONS
                                                :href "https://ratemydiy.github.io/Documentation/Architecture.html"} "Documentation"])
+          [:a {:class project-link :href (:repo project)} "Repo"]                                    
           [:div.tracked-ns.tc "Tech Used"
            (for [item (:tech-used project)] 
             [:div.hover-washed-blue.hover-bg-moon-gray.hover-animate item])]]])]])
